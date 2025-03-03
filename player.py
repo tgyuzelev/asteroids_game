74f6bfabfb7fbf9dd62cfbda931a5779206f658e
tgyuzelev <tgyuzelev@gmail.com>
2025-03-03 11:35:52 +0200

    Add shot rate limit

--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.__shot_cooldown = 0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -27,6 +28,7 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
     
     def update(self, dt):
+        self.__shot_cooldown -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -41,6 +43,10 @@ class Player(CircleShape):
             self.shoot()
     
     def shoot(self):
+        if self.__shot_cooldown > 0:
+            return
+
+        self.__shot_cooldown = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = (pygame.Vector2(0, 1).rotate(self.rotation)) * PLAYER_SHOOT_SPEED
     
