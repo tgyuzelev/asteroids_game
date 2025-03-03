1cb69339b1101ead4c4b2027f92a42a9dcabc638
tgyuzelev <tgyuzelev@gmail.com>
2025-03-03 10:26:50 +0200

    Add and draw player object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -8,14 +9,17 @@ def main():
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill('black')
-        pygame.display.flip()
+        player.draw(screen)
 
+        pygame.display.flip()
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
