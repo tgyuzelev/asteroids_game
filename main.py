acf2e8b9c1da24e7c261e84a21c150cdbfdc816a
tgyuzelev <tgyuzelev@gmail.com>
2025-03-03 11:03:30 +0200

    Spawn asteroids on map

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -9,19 +11,31 @@ def main():
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill('black')
-        player.draw(screen)
+
+        for draw_obj in drawable:
+            draw_obj.draw(screen)
+
         pygame.display.flip()
-        
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
